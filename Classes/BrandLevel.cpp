69acc5a069fbc9d24ccae75da64977f38453ffb3
MokrySzczupak <norbi2790@gmail.com>
2020-10-12 18:14:06 +0200

    Added functions that adds essential elements to brandLevel scene (especially addEssentialElements())

--- Classes/BrandLevel.cpp
+++ Classes/BrandLevel.cpp
@@ -12,6 +12,42 @@ bool BrandLevel::init()
 	{
 		return false;
 	}
-	
+	this->addEssentialElements();
 	return true;
+}
+void BrandLevel::addEssentialElements()
+{
+	addBackground("backgrounds/firstSceneBG.png");
+	addHeroFace("combatScene/hudRyze.png");
+	addHud();
+}
+void BrandLevel::addBackground(std::string backgroundFilePath)
+{
+	auto background = Sprite::create(backgroundFilePath);
+	background->setPosition(Vec2(320, 650));
+	addChild(background);
+}
+void BrandLevel::addHeroFace(std::string heroFaceFilePath)
+{
+	auto heroFace = Sprite::create(heroFaceFilePath);
+	heroFace->setPosition(Vec2(74, 126));
+	addChild(heroFace);
+}
+void BrandLevel::addHud()
+{
+	auto hud = Sprite::create("combatScene/hud.png");
+	hud->setPosition(Vec2(320, 128));
+	addChild(hud);
+	addHeroSpells("ryze");
+}
+void BrandLevel::addHeroSpells(std::string nameOfHero)
+{
+	auto spellOneIcon = MenuItemImage::create("combatScene/spells/ryzeSpell1.png", "combatScene/spells/" + nameOfHero + "Spell1.png");
+	auto spellTwoIcon = MenuItemImage::create("combatScene/spells/ryzeSpell2.png", "combatScene/spells/" + nameOfHero + "Spell2.png");
+	spellOneIcon->setPosition(Vec2(-30, -436));
+	spellTwoIcon->setPosition(Vec2(+150, -436));
+	auto spellsIconMenu = Menu::create();
+	spellsIconMenu->addChild(spellOneIcon);
+	spellsIconMenu->addChild(spellTwoIcon);
+	addChild(spellsIconMenu);
 }
--- Classes/BrandLevel.h
+++ Classes/BrandLevel.h
@@ -1,5 +1,6 @@
 #ifndef __BRAND_LEVEL_H__
 #define __BRAND_LEVEL_H__
+#include <string>
 #include "cocos2d.h"
 USING_NS_CC;
 class BrandLevel : public cocos2d::Node
@@ -8,5 +9,11 @@ public:
 	virtual bool init();
 	CREATE_FUNC(BrandLevel);
 	static Scene* createScene();
+private:
+	void addEssentialElements();
+	void addBackground(std::string backgroundFilePath);
+	void addHeroFace(std::string heroFaceFilePath);
+	void addHud();
+	void addHeroSpells(std::string nameOfHero);
 };
 #endif
